6d0c298d857004f2e0bc8ffd4b32db60a42535f2
genie2v <wkdwlsdl000@gmail.com>
2022-02-19 00:05:25 +0900

    백준10818번

---
+++ Algorithm/src/Baekjoon/BJ10818.java
@@ -0,0 +1,35 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class BJ10818 {
+
+//	N개의 정수가 주어진다. 이때, 최솟값과 최댓값을 구하는 프로그램을 작성하시오.
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+
+		int n = scanner.nextInt();
+		int[] number = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			number[i] = scanner.nextInt();
+		}
+
+		int minimum = number[0];
+		int maximun = number[0];
+
+		for (int i = 1; i < n; i++) {
+			if (minimum > number[i])
+				minimum = number[i];
+			if (maximun < number[i])
+				maximun = number[i];
+		}
+		
+		System.out.println(minimum + " " + maximun);
+
+		scanner.close();
+	}
+
+}
