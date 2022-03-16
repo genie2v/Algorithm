699655a7679587178d7ffc02f753a055aa92e2ec
genie2v <wkdwlsdl000@gmail.com>
2022-03-16 21:18:48 +0900

    백준15650번-백트래킹

---
+++ Algorithm/src/Baekjoon/BJ15650.java
@@ -0,0 +1,48 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ15650 {
+//	자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+//	- 1부터 N까지 자연수 중에서 중복 없이 M개를 고른 수열
+//	- 고른 수열은 오름차순이어야 한다.
+
+	public static int[] arr;
+	public static int n, m;
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+		n = Integer.parseInt(stringTokenizer.nextToken());
+		m = Integer.parseInt(stringTokenizer.nextToken());
+
+		arr = new int[m];
+
+		dfs(1, 0);
+
+		System.out.println(sb);
+
+	}
+
+	public static void dfs(int at, int depth) {
+		if (m == depth) {
+			for (int num : arr) {
+				sb.append(num).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+
+		for (int i = at; i <= n; i++) {
+			arr[depth] = i;
+			dfs(i + 1, depth + 1);
+		}
+	}
+
+}
