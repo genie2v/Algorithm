f98abc85f239aaeea77bab9b4288446931fb0a36
genie2v <wkdwlsdl000@gmail.com>
2022-03-16 21:29:04 +0900

    백준15651번-백트래킹

---
+++ Algorithm/src/Baekjoon/BJ15651.java
@@ -0,0 +1,47 @@
+package Baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ15651 {
+//	자연수 N과 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오.
+//	- 1부터 N까지 자연수 중에서 M개를 고른 수열
+//	- 같은 수를 여러 번 골라도 된다.
+
+	public static int[] arr;
+	public static int n, m;
+	public static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+
+		n = Integer.parseInt(stringTokenizer.nextToken());
+		m = Integer.parseInt(stringTokenizer.nextToken());
+
+		arr = new int[m];
+		dfs(0);
+
+		System.out.println(sb);
+
+	}
+
+	public static void dfs(int depth) {
+		if (m == depth) {
+			for (int num : arr) {
+				sb.append(num).append(' ');
+			}
+			sb.append('\n');
+			return;
+		}
+
+		for (int i = 1; i <= n; i++) {
+			arr[depth] = i;
+			dfs(depth + 1);
+		}
+	}
+
+}
